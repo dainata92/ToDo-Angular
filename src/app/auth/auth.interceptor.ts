7a15e04e9cf2197dddd9879bff78fd3d03fee81d
Dainata Leva <dainataleva@gmail.com>
2025-08-07 13:40:29 +0200

    MAJ interceptor

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = sessionStorage.getItem('authToken');
 
-  if (token && !req.url.endsWith('/api/action')) {
+  if (token) {
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
